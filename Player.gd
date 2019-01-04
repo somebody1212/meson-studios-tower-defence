2cd6da516e70e908df03335aefea486324974854
agm <agm.114.email@gmail.com>
2019-01-04 11:08:31 -0500

    Implemented Proper Jump Mechanics
    Implemented gravity, collisions etc.

--- Player.gd
+++ Player.gd
@@ -1,21 +1,33 @@
 extends KinematicBody2D
 
+const SPEED = 60
+const GRAVITY = 10
+const JUMP_POWER = -250
+const FLOOR = Vector2(0, -1)
+
 var velocity = Vector2()
+
+var on_ground = false
 
 func _physics_process(delta):
 	
 	if Input.is_action_pressed("ui_right"):
-		velocity.x = 30
+		velocity.x = SPEED
 	elif Input.is_action_pressed("ui_left"):
-		velocity.x = -30
+		velocity.x = -SPEED
 	else:
 		velocity.x = 0
 	
 	if Input.is_action_pressed("ui_up"):
-		velocity.y = -30
-	elif Input.is_action_pressed("ui_down"):
-		velocity.y = 30
-	else:
-		velocity.x = 0
+		if on_ground == true:
+			velocity.y = JUMP_POWER
+			on_ground = false
 	
-	move_and_slide(velocity)
+	velocity.y += GRAVITY
+	
+	if is_on_floor():
+		on_ground = true
+	else:
+		on_ground = false
+	
+	velocity = move_and_slide(velocity, FLOOR)
